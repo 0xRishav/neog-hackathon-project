d3a124cf646ad9d91cc8e6589e443eb36376f0c2
ritikadhiman <ritikadhimanritz@gmail.com>
2021-05-01 19:40:10 +0530

    feat: firebase authentication added

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,8 +1,20 @@
+import { useAuth } from "../context /authContext";
+import { signInWithGoogle } from "../services/auth";
+
 const LandingPage = () => {
+  const {setUser} = useAuth();
+
+  const signInClickHandler = async () => {
+    const signedInUser = await signInWithGoogle();
+		if (signedInUser) {
+			setUser(signedInUser);
+		}
+  }
+
   return (
     <div className="flex justify-center items-center flex-col h-screen">
       <h1 className="font-bold text-xl">Hey, Welcome to SocialHouse 🎉</h1>
-      <button className="px-4 py-1 bg-blue-500 rounded-md my-6">Sign In</button>
+      <button className="px-4 py-1 bg-blue-500 rounded-md my-6" onClick={signInClickHandler}>Sign In</button>
     </div>
   );
 };
