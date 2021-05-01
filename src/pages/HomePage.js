525549921abd7130a8574674f5adcdb2ff5031f0
Rishav Bharti <rshvbharti@gmail.com>
2021-05-01 10:29:36 +0530

    Created different component for room card

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,3 +1,5 @@
+import { RoomCard } from "../components";
+
 const HomePage = () => {
   const dummyRoom = {
     roomName: "Test Room",
@@ -40,81 +42,19 @@ const HomePage = () => {
       <div className="mt-4">
         <h1 className="font-bold my-6">Active Rooms</h1>
         {dummyRooms.map((dummyRoom, index) => (
-          <div
+          <RoomCard
+            dummyRoom={dummyRoom}
             key={index}
-            className="bg-white p-6 cursor-pointer rounded-xl my-6"
-          >
-            <div className="flex justify-between items-center w-full mb-4">
-              {dummyRoom.roomName}
-              <p>{dummyRoom.participants}</p>
-            </div>
-            <div className="flex flex-wrap items-start flex-col">
-              <div className="flex items-center">
-                {dummyRoom.speakers.map((speaker, index) => (
-                  <div className="">
-                    <img
-                      src={speaker.img}
-                      alt="speaker-image"
-                      key={index}
-                      className="h-8 w-8 object-cover rounded-md mx-1"
-                    />
-                  </div>
-                ))}
-              </div>
-              <div className="flex items-center mt-2">
-                {dummyRoom.speakers.map((speaker, index) => (
-                  <div className="">
-                    <span
-                      key={index}
-                      className="h-8 w-8 object-cover rounded-md mx-1"
-                    >
-                      {`${speaker.name}`}
-                      {index < TotalSpeakers - 1 ? "," : ""}
-                    </span>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
+            TotalSpeakers={TotalSpeakers}
+          />
         ))}
         <h1 className="font-bold my-6 mt-12">Saved Rooms</h1>
         {dummyRooms.map((dummyRoom, index) => (
-          <div
+          <RoomCard
+            dummyRoom={dummyRoom}
             key={index}
-            className="bg-white p-6 cursor-pointer rounded-xl my-6"
-          >
-            <div className="flex justify-between items-center w-full mb-4">
-              {dummyRoom.roomName}
-              <p>{dummyRoom.participants}</p>
-            </div>
-            <div className="flex flex-wrap items-start flex-col">
-              <div className="flex items-center">
-                {dummyRoom.speakers.map((speaker, index) => (
-                  <div className="">
-                    <img
-                      src={speaker.img}
-                      alt="speaker-image"
-                      key={index}
-                      className="h-8 w-8 object-cover rounded-md mx-1"
-                    />
-                  </div>
-                ))}
-              </div>
-              <div className="flex items-center mt-2">
-                {dummyRoom.speakers.map((speaker, index) => (
-                  <div className="">
-                    <span
-                      key={index}
-                      className="h-8 w-8 object-cover rounded-md mx-1"
-                    >
-                      {`${speaker.name}`}
-                      {index < TotalSpeakers - 1 ? "," : ""}
-                    </span>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
+            TotalSpeakers={TotalSpeakers}
+          />
         ))}
       </div>
     </div>
