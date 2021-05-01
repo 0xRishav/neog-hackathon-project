fc38ce0b0cf6a2252084b37b9551912bcb0421f9
Rishav Bharti <rshvbharti@gmail.com>
2021-05-01 07:40:34 +0530

    Created landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
+import { LandingPage } from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="font-bold text-xl">Hey, Welcome to SocialHouse 🎉</h1>
-      <button className="px-2 py-1 bg-blue-500 rounded-md">Sign In</button>
+    <div className="App bg-gray-100">
+      <LandingPage />
     </div>
   );
 }
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,10 @@
+const LandingPage = () => {
+  return (
+    <div className="flex justify-center items-center flex-col h-screen">
+      <h1 className="font-bold text-xl">Hey, Welcome to SocialHouse 🎉</h1>
+      <button className="px-4 py-1 bg-blue-500 rounded-md my-6">Sign In</button>
+    </div>
+  );
+};
+
+export default LandingPage;
