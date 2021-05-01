9e26ff9fe071c023403f3dd727390ce541f1d1df
Rishav Bharti <rshvbharti@gmail.com>
2021-05-01 07:24:49 +0530

    cleaned cra and added tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="font-bold text-xl">Hey, Welcome to SocialHouse 🎉</h1>
+      <button className="px-2 py-1 bg-blue-500 rounded-md">Sign In</button>
     </div>
   );
 }
