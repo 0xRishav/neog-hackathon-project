63cdb3c50eb36e5177a91b2ca102186139cfe89c
Rishav Bharti <rshvbharti@gmail.com>
2021-05-03 02:34:18 +0530

    fix: homepage image size

--- src/components/RoomCard.js
+++ src/components/RoomCard.js
@@ -1,9 +1,9 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { db, auth } from "../firebase";
-import { checkIfExists } from '../utils';
+import { checkIfExists } from "../utils";
 
 const RoomCard = ({ room, TotalSpeakers }) => {
-  const chatRoomRef = db.collection('chatRooms');
+  const chatRoomRef = db.collection("chatRooms");
 
   const addParticipantClickHandler = async (room) => {
     try {
@@ -16,7 +16,7 @@ const RoomCard = ({ room, TotalSpeakers }) => {
           isHandRaised: false,
           isOnStage: false,
           name: displayName,
-          photoUrl: photoURL
+          photoUrl: photoURL,
         });
         console.log("updating..", participants);
         await chatRoomRef.doc(room.id).update({ participants: participants });
@@ -26,40 +26,50 @@ const RoomCard = ({ room, TotalSpeakers }) => {
       console.log(err.message);
     }
   };
-  
+
   return (
     <Link to={`/room/${room.id}`}>
-    <div className="bg-white p-6 cursor-pointer rounded-2xl my-6" onClick={() => addParticipantClickHandler(room)}>
-      <div className="flex justify-between items-center w-full mb-4">
-        <h1>{room.topic}</h1>
-        <p>{room.participants.length}</p>
-      </div>
-      <div className="flex flex-wrap items-start flex-col">
-        <div className="flex items-center">
-          {room.participants.filter(item => item.isOnStage === true).map((item, index) => (
-            <div className="" key={index}>
-              <img
-                src={item.photoUrl}
-                alt="speaker"
-                className="h-8 w-8 object-cover rounded-2xl mx-1"
-              />
-            </div>
-          ))}
+      <div
+        className="bg-white p-6 cursor-pointer rounded-2xl my-6"
+        onClick={() => addParticipantClickHandler(room)}
+      >
+        <div className="flex justify-between items-center w-full mb-4">
+          <h1>{room.topic}</h1>
+          <p>{room.participants.length}</p>
         </div>
-        <div className="flex items-center mt-2">
-          {room.participants.filter(item => item.isOnStage === true).map((item, index) => (
-            <div className="" key={index}>
-              <span
-                className="h-8 w-8 object-cover rounded-2xl mx-1"
-              >
-                {`${item.name.split(" ", 1)}`}
-                {index < room.participants.filter(item => item.isOnStage === true).length - 1 ? "," : ""}
-              </span>
-            </div>
-          ))}
+        <div className="flex flex-wrap items-start flex-col">
+          <div className="flex items-center">
+            {room.participants
+              .filter((item) => item.isOnStage === true)
+              .map((item, index) => (
+                <div className="" key={index}>
+                  <img
+                    src={item.photoUrl}
+                    alt="speaker"
+                    className="h-4 w-4 object-cover rounded-2xl mx-1"
+                  />
+                </div>
+              ))}
+          </div>
+          <div className="flex items-center mt-2">
+            {room.participants
+              .filter((item) => item.isOnStage === true)
+              .map((item, index) => (
+                <div className="" key={index}>
+                  <span className="h-8 w-8 object-cover rounded-2xl mx-1 text-xs">
+                    {`${item.name.split(" ", 1)}`}
+                    {index <
+                    room.participants.filter((item) => item.isOnStage === true)
+                      .length -
+                      1
+                      ? ","
+                      : ""}
+                  </span>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
-    </div>
     </Link>
   );
 };
