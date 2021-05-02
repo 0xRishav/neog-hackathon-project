e76ce047e0663b5fcf4192bfeb7ef752a097973c
Rishav Bharti <rshvbharti@gmail.com>
2021-05-02 13:53:02 +0530

    created chat page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { ChatPage, HomePage, LandingPage } from "./pages";
 import { Routes, Route } from "react-router-dom";
 import { useAuth } from "./context/authContext";
 
-
 function App() {
-  const {user} = useAuth();
+  const { user } = useAuth();
   return (
     <div className="App">
       <div className="w-5/6 mx-auto my-0.5">
-          <Routes>
-            <Route path="/" element={user === null ? <LandingPage /> : <HomePage />} />
-           </Routes>
+        <Routes>
+          <Route
+            path="/"
+            element={user === null ? <LandingPage /> : <HomePage />}
+          />
+          <Route path="/room" element={<ChatPage />} />
+        </Routes>
       </div>
     </div>
   );
