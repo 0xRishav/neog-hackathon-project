8551081cce2b8b822931d62efda223811b852d4a
Rishav Bharti <rshvbharti@gmail.com>
2021-05-03 01:53:45 +0530

    style: hand raise UI

--- src/components/RoomParticipantsScreen.js
+++ src/components/RoomParticipantsScreen.js
@@ -6,21 +6,20 @@ import { useState, useEffect } from "react";
 import { auth, db } from "../firebase";
 import { useParams } from "react-router";
 
-const RoomParticipantsScreen = ({setIsOnChatScreen }) => {
+const RoomParticipantsScreen = ({ setIsOnChatScreen }) => {
   const [room, setRoom] = useState({});
-  const {roomId} = useParams();
+  const { roomId } = useParams();
 
   useEffect(() => {
     (async () => {
-      try{
-        const result = await db.collection('chatRooms').doc(roomId).get();
+      try {
+        const result = await db.collection("chatRooms").doc(roomId).get();
         setRoom(result.data());
-      }
-      catch(err){
+      } catch (err) {
         console.log(err);
       }
     })();
-  }, [])
+  }, []);
 
   console.log(room);
 
@@ -28,32 +27,52 @@ const RoomParticipantsScreen = ({setIsOnChatScreen }) => {
     <div>
       <h1 className="font-bold text-lg mb-4">Stage</h1>
       <div className="flex flex-wrap">
-        {room?.participants?.filter(item => item.isOnStage === true).map((item, index) => (
-          <div className="m-2" key={index}>
-            <img
-              className="h-10 w-10 object-cover rounded-2xl "
-              src={item.photoUrl}
-              alt="Speakerpic"
-            />
-            <p>{item.name.split(" ", 1)}</p>
-          </div>
-        ))}
+        {room?.participants
+          ?.filter((item) => item.isOnStage === true)
+          .map((item, index) => (
+            <div className="m-2 relative" key={index}>
+              {true && (
+                <div
+                  className="p-1 absolute bg-blue-500 rounded-full"
+                  style={{ top: "-6px", right: "-6px" }}
+                >
+                  <IoMdHand className="text-white" />
+                </div>
+              )}
+              <img
+                className="h-10 w-10 object-cover rounded-2xl "
+                src={item.photoUrl}
+                alt="Speakerpic"
+              />
+              <p>{item.name.split(" ", 1)}</p>
+            </div>
+          ))}
       </div>
 
       <div className="w-screen h-0.5 bg-gray-100" />
 
       <h1 className="font-bold text-lg my-4">Participants</h1>
       <div className="flex flex-wrap">
-        {room?.participants?.filter(item => item.isOnStage === false).map((item, index) => (
-          <div className="m-2" key={index}>
-            <img
-              className="h-10 w-10 object-cover rounded-2xl "
-              src={item.photoUrl}
-              alt="Participantpic"
-            />
-            <p>{item.name.split(" ", 1)}</p>
-          </div>
-        ))}
+        {room?.participants
+          ?.filter((item) => item.isOnStage === false)
+          .map((item, index) => (
+            <div className="m-2 relative" key={index}>
+              {true && (
+                <div
+                  className="p-1 absolute bg-blue-500 rounded-full"
+                  style={{ top: "-6px", right: "-6px" }}
+                >
+                  <IoMdHand className="text-white" />
+                </div>
+              )}
+              <img
+                className="h-10 w-10 object-cover rounded-2xl "
+                src={item.photoUrl}
+                alt="Participantpic"
+              />
+              <p>{item.name.split(" ", 1)}</p>
+            </div>
+          ))}
       </div>
       <div className="flex justify-start items-center fixed bottom-12">
         <button className="flex items-center px-4 py-1 bg-blue-500 rounded-2xl my-6">
